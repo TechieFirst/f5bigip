45c5e84132919c3d465b9f706991959c0c480910
Techie First <Techie.first87@gmail.com>
2025-06-11 16:00:53 +0530

    updated package

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import os
+import f5_bigip
+
 from setuptools import setup, find_packages
+
+
+if 'rpm' not in os.getcwd():
+    with open('requirements.txt') as fh:
+        required = [x for x in fh.read().splitlines() if not x.startswith('#')]
+else:
+    required = []
 
 setup(
     name="f5_bigip",
-    version="0.1.0",
+    version=f5_bigip.__version__,
     author="Techie First",
     author_email="techie.first87@gmail.com",
     description="This Package contains F5 basic modules which can be used in VIP creation, VIP Validation, Pool Creation and Pool Modifications",
@@ -10,15 +20,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TechieFirst/f5bigip",
     packages=find_packages(),
-    install_requires=[
-        "requests>=2.0",
-        "f5-sdk==3.0.21",
-        "pyYaml",
-        "setuptools"
-    ],
+    install_requires=required,
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.6",
 )
